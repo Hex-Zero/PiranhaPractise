a1c1623a36e8533dab03c34b0ba664e0496ee7c6
Hex-Zero <hex0@live.com>
2020-03-10 20:10:28 +0000

    New BlockGroup

---
+++ PiranhaPractise/Models/BlockGroups/Core.cs
@@ -0,0 +1,13 @@
+﻿using Piranha.Extend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PiranhaPractise.Models.BlockGroups
+{
+    [BlockGroupType(Name = "Core", Category = "Block Group", Icon = "fas fa-images")]
+    public class Core : Piranha.Extend.BlockGroup
+    {
+    }
+}
