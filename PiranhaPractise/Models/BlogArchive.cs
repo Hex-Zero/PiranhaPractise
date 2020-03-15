e76bbaf10df3a92e9a1c9c9f93be927ab7a1d84a
Hex-Zero <hex0@live.com>
2020-03-15 20:06:10 +0000

    new field

--- PiranhaPractise/Models/BlogArchive.cs
+++ PiranhaPractise/Models/BlogArchive.cs
@@ -2,11 +2,12 @@ using PiranhaPractise.Models.Regions;
 using Piranha.AttributeBuilder;
 using Piranha.Extend;
 using Piranha.Models;
+using Piranha.Extend.Fields;
 
 namespace PiranhaPractise.Models
 {
     [PageType(Title = "Blog archive", UseBlocks = false, IsArchive = true)]
-    public class BlogArchive  : Page<BlogArchive>
+    public class BlogArchive : Page<BlogArchive>
     {
         /// <summary>
         /// Gets/sets the archive hero.
@@ -18,5 +19,8 @@ namespace PiranhaPractise.Models
         /// Gets/sets the post archive.
         /// </summary>
         public PostArchive<DynamicPost> Archive { get; set; }
+
+        [Field(Options = FieldOption.HalfWidth)]
+        public StringField SubTitle { get; set; }
     }
 }
