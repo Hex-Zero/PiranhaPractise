43858712294f67c9b791b438ca8324e8d982d2bd
Hex-Zero <hex0@live.com>
2020-03-17 20:47:36 +0000

    Instantiate menu

--- PiranhaPractise/Models/SiteType/Permanents.cs
+++ PiranhaPractise/Models/SiteType/Permanents.cs
@@ -7,6 +7,9 @@ using PiranhaPractise.Models.Regions;
 [SiteType(Title = "Permanents")]
 public class Permanents : SiteContent<Permanents>
 {
+    [Region(Title = "Menu", Display = RegionDisplayMode.Setting)]
+    public Footer Menu { get; set; }
+
     [Region(Title = "Footer", Display = RegionDisplayMode.Setting)]
     public Footer Footer { get; set; }
 }
