c9e42c53079bd506d4a0feaae962117d90eec55e
Hex-Zero <hex0@live.com>
2020-03-17 20:45:47 +0000

    Menu Region

---
+++ PiranhaPractise/Models/Regions/Menu.cs
@@ -0,0 +1,51 @@
+using Piranha.Extend;
+using Piranha.Extend.Fields;
+using Piranha.Models;
+
+namespace PiranhaPractise.Models.Regions
+{
+    /// <summary>
+    /// Simple region for a teaser.
+    /// </summary>
+    public class Menu
+    {
+        /// <summary>
+        /// Gets/sets the main title.
+        /// </summary>
+        [Field(Options = FieldOption.HalfWidth)]
+        public StringField Title { get; set; }
+
+        /// <summary>
+        /// Gets/sets the optional subtitle.
+        /// </summary>
+        [Field(Options = FieldOption.HalfWidth)]
+        public StringField SubTitle { get; set; }
+
+        [Field(Options = FieldOption.HalfWidth)]
+        public StringField Description { get; set; }
+
+        /// <summary>
+        /// Gets/sets the optional page link.
+        /// </summary>
+        [Field(Title = "Optional Page link")]
+        public PageField PageLink { get; set; }
+
+        /// <summary>
+        /// Gets/sets the optional post link.
+        /// </summary>
+        [Field(Title = "Optional Post link")]
+        public PostField PostLink { get; set; }
+
+        /// <summary>
+        /// Gets/sets the main body.
+        /// </summary>
+        [Field]
+        public HtmlField Body { get; set; }
+
+        public Menu()
+        {
+            PageLink = new PageField();
+            PostLink = new PostField();
+        }
+    }
+}
